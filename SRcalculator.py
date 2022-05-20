398fdba660fe058a4487d3028212173045eae21a
Cailean Carter <cailean.carter@quadram.ac.uk>
2022-05-20 11:38:41 +0100

    Allows for inclusion of second elution column if first is empty

--- SRcalculator.py
+++ SRcalculator.py
@@ -8,7 +8,8 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description=__doc__)
 
     parser.add_argument("filepath", type=str)
-    parser.add_argument('-c', '--column', type=str, default='Column A')
+    parser.add_argument('-c', '--primary_col', type=str, default='Column A')
+    parser.add_argument('-s', '--secondary_col', type=str, default="Column B")
     parser.add_argument('-C', '--conc', type=float, default=5)
     parser.add_argument('-V', '--volume', type=float, default=10)
 
@@ -23,7 +24,11 @@ def calc_dilutions(conc):
     # final_conc = 5 #ng/uL
     # final_volume = 10 #uL
 
-    DNA_volume = (final_conc * final_volume) / conc
+    try:
+        DNA_volume = (final_conc * final_volume) / conc
+    except ZeroDivisionError:
+        DNA_volume = 0
+
     water_volume = 10 - DNA_volume
 
     try:
@@ -34,22 +39,27 @@ def calc_dilutions(conc):
     return round(DNA_volume, 2), round(water_volume, 2)
 
 
-
 if __name__ == '__main__':
 
     args = parse_arguments()
 
     fp = args.filepath
-    col = args.column
+    col1 = args.primary_col
+    col2 = args.secondary_col
     final_conc = args.conc
     final_volume = args.volume
 
-
     qubit = pd.read_excel(fp, index_col=0)
-    dilutions = qubit[col].apply(calc_dilutions)
+
+    dilutions = qubit[col1].apply(calc_dilutions)
     dilutions = dilutions.apply(pd.Series)
     dilutions.columns = ["DNA (uL)", "Water (uL)"]
     output = pd.concat([qubit, dilutions], axis=1)
-    output.to_excel(fp)
 
+    dilutions = output[output["DNA (uL)"] == 0][col2].apply(calc_dilutions)
+    dilutions = dilutions.apply(pd.Series)
+    dilutions.columns = ["DNA (uL)", "Water (uL)"]
+    output[output["DNA (uL)"] == 0] = pd.concat([qubit, dilutions], axis=1)
+
+    output.to_excel(fp)
     print("Done.")
